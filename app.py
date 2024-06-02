bc30455d9673d209940400731af10b2c5fece7b5
nishant129 <nishantborkar139@gmail.com>
2024-06-02 14:26:02 +0530

    update app.py

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ from src.main import crew,crew2
 from PIL import Image
 import torch
 from diffusers import StableDiffusionXLPipeline, DPMSolverSinglestepScheduler
+import os
 
+os.makedirs("output",exist_ok=True)
 # image = Image.open("")
 
 # # Load model.
-# pipe = StableDiffusionXLPipeline.from_pretrained("sd-community/sdxl-flash", torch_dtype=torch.float16).to("cuda")
-# pipe.scheduler = DPMSolverSinglestepScheduler.from_config(pipe.scheduler.config, timestep_spacing="trailing")
+pipe = StableDiffusionXLPipeline.from_pretrained("sd-community/sdxl-flash", torch_dtype=torch.float16).to("cuda")
+pipe.scheduler = DPMSolverSinglestepScheduler.from_config(pipe.scheduler.config, timestep_spacing="trailing")
 
 
 st.title("Social Media Blog and Post")
@@ -27,14 +29,13 @@ if prompt := st.chat_input("Write Your Topic"):
         
     st.session_state.messages.append({"role": "assistant", "content":prompt})
     text = {"topic": prompt}
-    #result = crew.kickoff(inputs=text)
-    result = "ok" 
+    result = crew.kickoff(inputs=text) 
     result2 = crew2.kickoff(inputs=text)
     
     responce = f"Echo: {result}"
     split_prompts = [prompt.strip() for prompt in result2.split("\n") if result2.strip()]
-    # for i in split_prompts:
-    #     pipe(i, num_inference_steps=7, guidance_scale=3).images[0].save("output/output"+f"{i[0:1]}"+".png")
+    for i in split_prompts:
+        pipe(i, num_inference_steps=7, guidance_scale=3).images[0].save("output/output"+f"{i[0:1]}"+".png")
     with st.chat_message(name="assistant"):
         st.markdown(responce)
         
@@ -42,12 +43,12 @@ if prompt := st.chat_input("Write Your Topic"):
     image = Image.open("output/output.png")
     image1 = Image.open("output/output2.png")
     image2 = Image.open("output/output3.png")
-    # image3 = Image.open("output/output4.png")
-    # image4 = Image.open("output/output5.png")
+    image3 = Image.open("output/output4.png")
+    image4 = Image.open("output/output5.png")
     st.image(image,width=200)
     st.image(image1,width=200)
     st.image(image2,width=200)
-    # st.image(image=image3, width=200)
-    # st.image(image=image4, width=200)
+    st.image(image=image3, width=200)
+    st.image(image=image4, width=200)
 # with st.chat_message(name="assistant"):
 #     st.write("Hello")output
